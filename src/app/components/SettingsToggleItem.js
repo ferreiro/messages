5532af89d9d31841a4117d17709ee004d5b5d0fb
Jorge Ferreiro <jorge@ferreiro.me>
2018-08-27 08:31:44 +0200

    Improved toggle UI on dark mode

--- src/app/components/SettingsToggleItem.js
+++ src/app/components/SettingsToggleItem.js
@@ -19,7 +19,7 @@ class SettingsToggleItem extends Component {
 	        >	
 	        	<button
 	        		onClick={this.handleClickEvent}
-	        		style={{width: '100%', height: '100%', background: 'transparent', outline: 0, border: 0, 'z-index': 1, position: 'absolute', top: 0, left: 0}}></button>
+	        		style={{width: '100%', height: '100%', background: 'transparent', outline: 0, border: 0, zIndex: 1, position: 'absolute', top: 0, left: 0}}></button>
 	            <div className="search__history__icon flexbox">
 	                <span className={'icon ' + icon}></span>
 	            </div>
